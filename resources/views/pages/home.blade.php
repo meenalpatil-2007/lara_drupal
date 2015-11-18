aa3d28025dc8c3554f949944dca1755f9e8592e2
Atif Shaikh <shaikh388@gmail.com>
2015-11-18 13:19:04 +0530

    design changes

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,63 +9,63 @@
 <div class="container bootstrap snippet">
 test
 	<div class="row">
-	<div class="jumbotron list-content">
-    <ul class="list-group">
-      <li  class="list-group-item title">
-        Recommended Matches 
-        <div class="heart-divider">
-			<span class="grey-line"></span>
-			<i class="fa fa-heart pink-heart"></i>
-			<i class="fa fa-heart grey-heart"></i>
-			<span class="grey-line"></span>
+		<div class="list-content col-md-4">
+		    <ul class="list-group">
+		      	<li  class="list-group-item title">
+			        Recommended Matches 
+			        <div class="heart-divider">
+						<span class="grey-line"></span>
+						<i class="fa fa-heart pink-heart"></i>
+						<i class="fa fa-heart grey-heart"></i>
+						<span class="grey-line"></span>
+					</div>
+			    </li>
+		      	<li  class="list-group-item text-left">
+			      	<a href="#" class="name">
+				        {!! Html::image('/assets/plugins/bootstrap/img/1.jpg', 'a picture', ['class' => 'img-thumbnail']) !!}
+				        Juan guillermo cuadrado
+			      	</a>
+			        <label class="pull-right">
+			            <!--a  class="btn btn-success btn-xs glyphicon glyphicon-ok"  title="View"></a>
+			            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash"  title="Delete"></a-->
+			            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment"  title="Send message"></a>
+			        </label>
+			        <div class="break"></div>
+		      	</li>
+			    <li  class="list-group-item text-left">
+			      	<a href="#" class="name">
+			          {!! Html::image('/assets/plugins/bootstrap/img/2.jpg', 'a picture', ['class' => 'img-thumbnail']) !!}
+			          James Rodriguez (10)
+			        </a>
+			        <label class="pull-right">
+			            <!--a  class="btn btn-success btn-xs glyphicon glyphicon-ok"  title="View"></a>
+			            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash"  title="Delete"></a-->
+			            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment"  title="Send message"></a>
+			        </label>
+			        <div class="break"></div>
+			    </li>
+			    <li  class="list-group-item text-left">
+			     	<a href="#" class="name">
+			          	{!! Html::image('/assets/plugins/bootstrap/img/3.jpg', 'a picture', ['class' => 'img-thumbnail']) !!}
+			            Mariana pajon
+			        </a>
+			        <label class="pull-right">
+			            <!--a  href="#" class="btn btn-success btn-xs glyphicon glyphicon-ok"  title="View"></a>
+			            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash"  title="Delete"></a-->
+			            <a  href="#" class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment"  title="Send message"></a>
+			        </label>
+			        <div class="break"></div>
+			    </li>
+			    <li  class="list-group-item text-left">
+			        <a class="btn btn-block btn-primary">
+			            <i class="glyphicon glyphicon-refresh"></i>
+			            Load more...
+			        </a>
+			    </li>
+		    </ul>
 		</div>
-      </li>
-      <li  class="list-group-item text-left">
-      <a href="#" class="name">
-        {!! Html::image('/assets/plugins/bootstrap/img/1.jpg', 'a picture', ['class' => 'img-thumbnail']) !!}
-            Juan guillermo cuadrado
-        </a>
-        <label class="pull-right">
-            <!--a  class="btn btn-success btn-xs glyphicon glyphicon-ok"  title="View"></a>
-            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash"  title="Delete"></a-->
-            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment"  title="Send message"></a>
-        </label>
-        <div class="break"></div>
-      </li>
-      <li  class="list-group-item text-left">
-      <a href="#" class="name">
-          {!! Html::image('/assets/plugins/bootstrap/img/2.jpg', 'a picture', ['class' => 'img-thumbnail']) !!}
-          James Rodriguez (10)
-        </a>
-        <label class="pull-right">
-            <!--a  class="btn btn-success btn-xs glyphicon glyphicon-ok"  title="View"></a>
-            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash"  title="Delete"></a-->
-            <a  class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment"  title="Send message"></a>
-        </label>
-        <div class="break"></div>
-      </li>
-      <li  class="list-group-item text-left">
-      <a href="#" class="name">
-          {!! Html::image('/assets/plugins/bootstrap/img/3.jpg', 'a picture', ['class' => 'img-thumbnail']) !!}
-            Mariana pajon
-          </a>
-        <label class="pull-right">
-            <!--a  href="#" class="btn btn-success btn-xs glyphicon glyphicon-ok"  title="View"></a>
-            <a  class="btn btn-danger  btn-xs glyphicon glyphicon-trash"  title="Delete"></a-->
-            <a  href="#" class="btn btn-info  btn-xs glyphicon glyphicon glyphicon-comment"  title="Send message"></a>
-        </label>
-        <div class="break"></div>
-      </li>
-      <li  class="list-group-item text-left">
-        <a class="btn btn-block btn-primary">
-            <i class="glyphicon glyphicon-refresh"></i>
-            Load more...
-        </a>
-      </li>
-    </ul>
-  </div>
-  </div>
-  </div>
+
+	</div>
 </div>
 
 @endsection
