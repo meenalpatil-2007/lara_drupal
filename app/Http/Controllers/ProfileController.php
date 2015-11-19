abf512613d41b7ce469f4056e2e667de7087e0ee
meenalpatil-2007 <meenalpatil.2007@gmail.com>
2015-11-19 18:25:07 +0530

    indentation

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -33,22 +33,15 @@ class ProfileController extends Controller
 			return $res;
 		} else {
 			$response = json_decode($res);
-
-			/* PAGINATION CODE */
 			$page = isset($userData['page'])?$userData['page']:0;
 			$pagination = Common::getPagination($page, $response);
 			$pagination['url'] = Common::curPageURL();
-			//Common::pr($pagination); exit;
 			$paginationUI = Common::getPaginationUI($pagination);
-			//combine=aks&field_gender_value=female&field_religion_value=All&field_living_location_value%5B%5D=mumbai
 			return view('pages.search', array('userData' => $userData, 'response' => $response, 'paginationUI' => $paginationUI));
 		}
 	}
 	
 	function getMyProfile($action=null){
-		//echo "<pre>"; print_r($this->request->session()->all());
-		//print_r($this->request->session()->get('uid'));
-		//exit;
 		$url = $this->site_url	.'/m2serve/my-profile?user='.$this->request->session()->get('uid');		
 		$userDate='';	
 		
@@ -58,11 +51,8 @@ class ProfileController extends Controller
 		if(is_object($output)) {
 			return $output;
 		} else {
-		$array = json_decode($output,TRUE);
-
-		//Common::pr($array); exit;	
-		
-			if(empty($array)){			
+			$array = json_decode($output,TRUE);
+			if(empty($array)){
 				$view = view('profile.edit_profile', array('item'=>$array));
 			}else{
 				$view = view('profile.my_profile', array('item'=>$array[0]));
@@ -71,9 +61,7 @@ class ProfileController extends Controller
 			if($action == "edit"){
 				$view = view('profile.edit_profile', array('item'=>$array));						
 			}
-		
-		}	
-		
+		}
 		return $view;
 	}
 
@@ -83,6 +71,5 @@ class ProfileController extends Controller
 		
 		$res = Common::appendParamToUrlNew($appendParams, $url);
 		print_r($res);
-		//http://laravel5.dev/profile/search?combine=name&field_living_location_value=pune&field_gender_value=All&field_religion_value=All&page=20&exists=yes
 	}
 }
